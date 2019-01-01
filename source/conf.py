d9dbd064c3b977f79db180bf4489ac0b46a9ff53
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-01-01 14:59:24 +0100

    Try to remove numfig.

--- source/conf.py
+++ source/conf.py
@@ -35,7 +35,6 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.numfig',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
